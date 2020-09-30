f9c03a974a7a51afc92e648259efa0c47ef552d8
mquires <evg.tulya@gmail.com>
2020-09-30 12:57:41 +0300

    feat: add min max avg functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+  if (array && array.length) {
+    return Math.min(...array);
+  } else {
+    return 0;
+  }
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+  if (array && array.length) {
+    return Math.max(...array);
+  } else {
+    return 0;
+  }
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+  if (array && array.length) {
+    const total = array.reduce((total, amount) => total += amount);
+    return total / array.length;
+  } else {
+    return 0;
+  }
 }
